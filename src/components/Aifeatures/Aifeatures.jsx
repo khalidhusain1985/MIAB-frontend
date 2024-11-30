6ebe2efda92d81500df83e7cc1ebfe9681f43edc
K2k-WebDeveloper <itsupport1@k2kitsupport.com>
2024-11-30 21:42:26 +0530

    update cards

--- src/components/Aifeatures/Aifeatures.jsx
+++ src/components/Aifeatures/Aifeatures.jsx
@@ -101,22 +101,7 @@ export function Aifeatures() {
                 (Provided some Emojis)
               </span>
             </li>
-            <li className="flex items-center">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-teal-700 "
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-black ms-3">
-                Media sharing
-                <br/>
-                (Can share Media within 10mb)
-              </span>
-            </li>
+            
             <li className="flex items-center">
               <svg
                 className="flex-shrink-0 w-4 h-4 text-teal-700 "
@@ -219,22 +204,7 @@ export function Aifeatures() {
                 (Ai filters)
               </span>
             </li>
-            <li className="flex items-center">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-teal-700 "
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-black ms-3">
-                Media sharing
-                <br/>
-                (Can share Media within 10mb)
-              </span>
-            </li>
+            
             <li className="flex items-center">
               <svg
                 className="flex-shrink-0 w-4 h-4 text-teal-700 "
